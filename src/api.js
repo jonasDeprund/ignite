ed310d07ffbbfa7450a7d79e9df159d6c6a21ce9
Jonas Deprund <jonasdeprund@gmail.com>
2021-06-22 19:08:12 +0200

    added data done

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 // Base url
-const base_url = "https://api.rawg.io/api/";
+const base_url = 'https://api.rawg.io/api/';
 
 // Set date
 
@@ -29,7 +29,13 @@ const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
 const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
-// Popular Games
+//Popular Games
 const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const upcoming_games = `games?dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+const newGames = `games?dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
 
 export const popularGamesURL = () => `${base_url}${popular_games}`;
+export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
+export const newGamesURL = () => `${base_url}${newGames}`;
+
+console.log(upcomingGamesURL());
