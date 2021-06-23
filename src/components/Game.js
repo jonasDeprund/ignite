f20af094b696969665782d32630c1b7fdbba442a
Jonas Deprund <jonasdeprund@gmail.com>
2021-06-23 17:50:04 +0200

    security api done

---
+++ src/components/Game.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// Style and animation
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
+const Game = () => {
+  return (
+    <div>
+      <h3>Game name</h3>
+      <p>realease date</p>
+    </div>
+  );
+};
+
+export default Game;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,20 @@
 import react, { useEffect } from 'react';
 // Redux
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { loadGames } from '../actions/gamesAction';
+// Components
+import Game from '../components/Game';
+// Style and animation
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
 const Home = () => {
   // Fetch games
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(loadGames());
-  });
+  }, [dispatch]);
+  const { popular, newGames, upcoming } = useSelector((state) => state.games);
   return (
     <div>
       <h1>Home</h1>
