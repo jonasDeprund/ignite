3e8dd3a8531956e4e35011fff7608242119601e5
jonasDeprund <jonasdeprund@gmail.com>
2021-07-01 11:53:31 +0200

    add load waiting function done

--- src/actions/detailAction.js
+++ src/actions/detailAction.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 import { GameDetailsURL, GameScreenshotURL } from '../api';
 
 export const loadDetail = (id) => async (dispatch) => {
+  dispatch({
+    type: 'LOADING_DETAIL',
+  });
   const detailData = await axios.get(GameDetailsURL(id));
   const screenShotData = await axios.get(GameScreenshotURL(id));
   dispatch({
