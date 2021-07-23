b064c265c1d6ffaa375cfd880e89a4bfa2173237
Jonas Deprund <jonasdeprund@gmail.com>
2021-07-23 22:16:06 +0200

    animations done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,6 +9,8 @@ import Game from '../components/Game';
 import styled from 'styled-components';
 import { motion, AnimatePresence, AnimateSharedLayout } from 'framer-motion';
 import { useLocation } from 'react-router-dom';
+// Animation
+import { fadeIn } from '../animations';
 
 const Home = () => {
   // Get the current location
@@ -23,7 +25,7 @@ const Home = () => {
     (state) => state.games
   );
   return (
-    <GameList>
+    <GameList variants={fadeIn} initial="hidden" animate="show">
       <AnimateSharedLayout type="crossfade">
         <AnimatePresence>
           {pathId && <GameDetail pathId={pathId} />}
